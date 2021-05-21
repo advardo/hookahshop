ecc0a0b1117820aeb4d98fe2501131c3993292b8
Eduard <advardo@ukr.net>
2021-05-21 09:04:21 +0300

    First initialization

---
+++ server/index.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+const express = require('express');
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.listen(PORT, ()=>{
+    console.log(`Server started at port ${PORT}`);
+})
